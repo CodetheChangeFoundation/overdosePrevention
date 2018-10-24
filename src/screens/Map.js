fabb639ad78bbfb8b4b417683cce8a335315b9cd
Samson Keung <samsonkeung@hotmail.com>
2018-10-23 23:02:48 -0700

    Show Google map on Map Screen

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -2,16 +2,26 @@ import {Button, Text, View} from "react-native";
 import React from "react";
 import ChooseCityScreen from "./ChooseCity";
 import PropTypes from "prop-types";
+import {MapView} from "expo";
 
 export default class MapScreen extends React.Component {
     static ScreenName = "MapScreen";
 
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Map Screen</Text>
+            <View style={{ flex: 1, justifyContent: 'center'}}>
+                <MapView
+                    style={{ flex: 1 }}
+                    provider="google"
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
                 <Button
-                    title="Map Screen"
+                    title="Go Back To Choose City Screen"
                     onPress={() => this.props.navigation.navigate(ChooseCityScreen.ScreenName)}
                 />
             </View>
