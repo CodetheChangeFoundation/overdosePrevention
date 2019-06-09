5d97f6a0c8a0a5317e3dfd3e075042ac81556e71
Pritpal Chauhan <pritpalc13@gmail.com>
2019-06-08 17:31:39 -0700

    OPS-13: Fixed tabbing

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 import SwipeUpSearch from '../components/SwipeUpSearch';
 import MapPopup from "../components/MapPopup";
 
-const deltas = {latitudeDelta: 0.0922, longitudeDelta: 0.0421};
+const deltas = {latitudeDelta: 0.0922, longitudeDelta: 0.0421}; // TODO: ??
 
 export default class MapScreen extends React.Component {
 	static navigationOptions = {
@@ -16,7 +16,7 @@ export default class MapScreen extends React.Component {
 	constructor(props) {
     super(props);
         
-    // initialRegion is San Francisco if no params are passed in
+    // TODO: not needed, shouldn't be San Fran initial region
     let initialRegion = this.props.navigation.getParam('coordinates',
       {
         latitude: 37.78825,
@@ -107,41 +107,43 @@ export default class MapScreen extends React.Component {
     return marker;
   }
 
+    // TODO: create a modal and complete functions below
   createSiteDescription(hours, street, province, postalCode, phoneNumber) {
-    // TODO: takes in a service's hours and address and formats it, calls formatContactInfo()
-    return `${street}, ${postalCode} ${province}\n${phoneNumber}\n${hours}`;
+    // TODO: takes in a service's hours and address and formats it
+    return `${street}, ${postalCode} ${province}\n${this.formatPhoneNumber(phoneNumber)}${hours}`;
   }
 
-  formatContactInfo(street, province, postalCode, phoneNumber) {
-    // TODO: takes in a service's address and number and formats it
-    // formats phone number
-    return `${street}, ${postalCode} ${province}\n${phoneNumber}`;
+  formatPhoneNumber(phoneNumber) {
+    // TODO: takes in a phone number and formats it
+    if (phoneNumber)
+    {
+      return `${phoneNumber}\n`;
+    }
   }
   
-  // TODO: create a modal
 	render() {
-		return (
-			<View style={{ flex: 1, justifyContent: 'center' }}>
-				{/* <Modal
-					visible={this.state.isPopupActive}
-					transparent={true}
-        >
-					<View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.3)'}}>
-						<View style={{margin: "10% 10% 10% 10%", backgroundColor: 'rgba(255,255,255, 0.9)'}}>
-							<MapPopup service={this.state.currentService}></MapPopup>
-							<View style={{alignItems: "center"}}>
-								<TouchableHighlight
-									onPress={() => {
-										this.serviceClick(null, false);
-									}}
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        {/* <Modal
+          visible={this.state.isPopupActive}
+          transparent={true}
+          >
+          <View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.3)'}}>
+            <View style={{margin: "10% 10% 10% 10%", backgroundColor: 'rgba(255,255,255, 0.9)'}}>
+              <MapPopup service={this.state.currentService}></MapPopup>
+              <View style={{alignItems: "center"}}>
+                <TouchableHighlight
+                  onPress={() => {
+                    this.serviceClick(null, false);
+                  }}
                 >
-									<Text>Hide Modal</Text>
-								</TouchableHighlight>
-							</View>
-						</View>
-					</View>
-				</Modal> */}
-
+                  <Text>Hide Modal</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </View>
+        </Modal> */}
+        
         {/* <SearchBar
           round={true}
           placeholder="Search for a place or address"
@@ -156,21 +158,21 @@ export default class MapScreen extends React.Component {
           searchIcon={null}
           clearIcon={null}
         /> */}
-
-				<MapView
-					style={{ flex: 1 }}
-					provider="google"
-					initialRegion={this.state.region}
-				>
+        
+        <MapView
+          style={{ flex: 1 }}
+          provider="google"
+          initialRegion={this.state.region}
+        >
           {this.renderServices()}
-				</MapView>		
-
+        </MapView>		
+        
         <SwipeUpSearch
           onLogoPress={this.changeRegion}
         />
-			</View>
-		);
-	}
+      </View>
+    );
+  }
 }
 
 MapScreen.propTypes = {
