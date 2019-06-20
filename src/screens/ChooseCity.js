1e7f5f90563f7bf6abacf686631edb2f9e2b3d5a
Viniel Kumar <vinielk@icloud.com>
2019-06-20 11:31:56 -0700

    OPS-34: started modal implementation

--- src/screens/ChooseCity.js
+++ src/screens/ChooseCity.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View, StyleSheet, Text, Dimensions, Picker } from "react-native";
-import { Location } from 'expo';
+import { Location, Permissions } from 'expo';
 import Toaster from 'react-native-toaster';
 import ResponsiveButton from '../components/ResponsiveButton';
 import OpsLogo from "../components/logos/OpsLogo";
@@ -118,30 +118,22 @@ export default class ChooseCityScreen extends React.Component {
   }
 
   enableLocationServices = async () => {
-    let self = this;
-    Location.requestPermissionsAsync();
-    let locationEnabled = await Location.hasServicesEnabledAsync();
-    if (locationEnabled) {
-      navigator.geolocation.watchPosition((position) => {
-        self.props.navigation.navigate('Map', {
-          isAnonymous: 0,
-          coordinates: position.coords,
-          services: this.state.sites
-        });
-      },
-      (error) => {
-        console.log(error);
-        this.setLocationFlag();
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        isAnonymous: 1,
+        locationDisabled: true
       });
-    } else {
-      this.setLocationFlag();
     }
-  }
 
-  setLocationFlag() {
-    this.setState({
-      isAnonymous: 1,
-      locationDisabled: true
+    let location = await Location.getCurrentPositionAsync({});
+    this.props.navigation.navigate('Map', {
+      isAnonymous: 0,
+      coordinates: {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      },
+      services: this.state.sites
     });
   }
 
--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { Dimensions, Text, View, StyleSheet, Button, Modal, TouchableHighlight, Image } from "react-native";
+import { Dimensions, Text, View, StyleSheet, Button, Modal, TouchableHighlight, TouchableWithoutFeedback, Image } from "react-native";
 import { MapView } from "expo";
 import { SearchBar } from 'react-native-elements';
 import PropTypes from "prop-types";
 import SwipeUpSearch from '../components/SwipeUpSearch';
 import MapPopup from "../components/MapPopup";
 import MapViewDirections from 'react-native-maps-directions';
+import ResponsiveButton from "../components/ResponsiveButton";
 
-const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
-const deltas = {latitudeDelta: 0.0922, longitudeDelta: 0.0421};
+const { width: WINDOW_WIDTH, height: WINDOW_HEIGHT } = Dimensions.get('window');
+const DELTAS = {latitudeDelta: 0.0922, longitudeDelta: 0.0421};
 const GOOGLE_MAPS_APIKEY = 'AIzaSyBO8JtI4QwXlt2khUX66l71yAi2hEKCsPo';
+const MARKER_IMAGES = {
+  "Supervised Injection": require('../../assets/needle_marker.png'),
+  "Replacement": require('../../assets/replacement_marker.png'),
+  "Pipe": require('../../assets/pipe_marker.png'),
+  "Nurse": require('../../assets/nurse_marker.png'),
+  "Mobile Unit": require('../../assets/mobile_unit_marker.png'),
+  "Detox": require('../../assets/detox_marker.png'),
+  "Default": require('../../assets/default_marker.png')
+}
 
 export default class MapScreen extends React.Component {
 	static navigationOptions = {
@@ -19,20 +29,21 @@ export default class MapScreen extends React.Component {
 	constructor(props) {
     super(props);
     const initialOrigin = this.props.navigation.getParam('coordinates');
-    let destination  = undefined;
-    let origin = undefined;
 
 		this.state = {
-      isAnonymous: this.props.navigation.getParam('isAnonymous'),
-      region: {...initialOrigin, ...deltas},
-      servicesToDisplay: undefined
+      region: {...initialOrigin, ...DELTAS},
+      servicesToDisplay: undefined,
+      modalVisible: false
     }
 
+    this.destination = undefined;
+    this.origin = undefined;
+    this.isAnonymous = this.props.navigation.getParam('isAnonymous'),
     this.filterSites = this.filterSites.bind(this);
     this.setDestination = this.setDestination.bind(this);
-	}
+  }
 
- filterSites(type) {
+  filterSites(type) {
     this.setState({
       servicesToDisplay: type
     });
@@ -42,9 +53,11 @@ export default class MapScreen extends React.Component {
     const { servicesToDisplay } = this.state;
     let sites = this.props.navigation.getParam('services');
 
-    if (this.destination !== undefined) {
-      sites = sites.filter(site => this.destination.latitude === parseFloat(site.lat) && this.destination.longitude === parseFloat(site.lon));
-    } else if (servicesToDisplay !== undefined) {
+    // if (this.destination !== undefined) {
+    //   sites = sites.filter(site => this.destination.latitude === parseFloat(site.lat) && this.destination.longitude === parseFloat(site.lon));
+    // } else 
+    
+    if (servicesToDisplay !== undefined) {
       sites = sites.filter(site => servicesToDisplay === site.service);
     }
 
@@ -57,50 +70,13 @@ export default class MapScreen extends React.Component {
               "latitude": parseFloat(site.lat),
               "longitude": parseFloat(site.lon)
             }}
-            title={site.name}
-            // onPress={() => this.serviceClick(site, true)}
-            image={this.setMapMarker(site.service)}
-            ref={ref => { this.marker = ref; }}
-          >
-            <MapView.Callout onPress={() => this.setDestination(site.lat, site.lon)}>
-              <Text>
-                {this.createSiteDescription(site.hours, site.street, site.province, site.postal_code, site.phone_number)}
-              </Text>
-              <Text style={styles.clickableText}>Directions</Text>
-            </MapView.Callout>
-          </MapView.Marker>
+            onPress={() => { this.destination = site; this.setState({modalVisible: true}) }}
+            image={MARKER_IMAGES[site.service] ? MARKER_IMAGES[site.service] : MARKER_IMAGES["Default"]}
+            // ref={ref => { this.marker = ref; }}
+          />
         );
       });
     }
-  }
-
-  setMapMarker(serviceType) {
-    let marker;
-
-    switch (serviceType) {
-      case "Supervised Injection":
-        marker = require('../../assets/needle_marker.png');
-        break;
-      case "Replacement":
-        marker = require('../../assets/replacement_marker.png');
-        break;
-      case "Pipe":
-        marker = require('../../assets/pipe_marker.png');
-        break;
-      case "Nurse":
-        marker = require('../../assets/nurse_marker.png');
-        break;
-      case "Mobile Unit":
-        marker = require('../../assets/mobile_unit_marker.png');
-        break;
-      case "Detox":
-        marker = require('../../assets/detox_marker.png');
-        break;
-      default:
-        // TODO: need a default marker
-    }
-
-    return marker;
   }
 
     // TODO: create a modal and complete functions below
@@ -117,41 +93,29 @@ export default class MapScreen extends React.Component {
     }
   }
   
-  setDestination(lat, lon) {
-    const destLat = parseFloat(lat);
-    const destLon = parseFloat(lon);
-    this.destination = { latitude: destLat, longitude: destLon }
-    
+  setDestination() {
+    const destLat = parseFloat(this.destination.lat);
+    const destLon = parseFloat(this.destination.lon);
+    // this.destination = { latitude: destLat, longitude: destLon }
+
     if (this.origin !== undefined) {
       const latitudeDelta = destLat > this.origin.latitude ? destLat - this.origin.latitude : this.origin.latitude - destLat;
       const longitudeDelta = destLon > this.origin.longitude ? destLon - this.origin.longitude : this.origin.longitude - destLon;
-      this.marker.hideCallout();
+      // this.marker.hideCallout();
       this.setState({
         region: {
           latitude: (destLat + this.origin.latitude) / 2,
           longitude: (destLon + this.origin.longitude) / 2,
-          latitudeDelta: latitudeDelta + ((200 * latitudeDelta) / windowHeight),
-          longitudeDelta: longitudeDelta + ((200 * longitudeDelta) / windowWidth)
+          latitudeDelta: latitudeDelta + ((250 * latitudeDelta) / WINDOW_HEIGHT),
+          longitudeDelta: longitudeDelta + ((250 * longitudeDelta) / WINDOW_WIDTH)
         }
       });
     }
   }
 
-  // renderCurrentLocation() {
-  //   return (
-  //     <MapView.Marker
-  //       anchor={{x: 0.5, y: 0.5}}
-  //       coordinate={{
-  //         "latitude": parseFloat(this.state.origin.latitude),
-  //         "longitude": parseFloat(this.state.origin.longitude)
-  //       }}
-  //       image={require('../../assets/current_location_marker.png')}
-  //     />
-  //   );
-  // }
-
 	render() {
-    const { isAnonymous, region } = this.state;
+    const { region, modalVisible } = this.state;
+
     return (
       <View style={{ flex: 1, justifyContent: 'center' }}>
         {/* <SearchBar
@@ -173,16 +137,21 @@ export default class MapScreen extends React.Component {
           style={{ flex: 1 }}
           provider="google"
           region={region}
-          showsUserLocation={!isAnonymous ? true : false}
-          followsUserLocation={!isAnonymous ? true : false}
-          onUserLocationChange={!isAnonymous ? (params) => this.origin = { latitude: params.nativeEvent.coordinate.latitude, longitude: params.nativeEvent.coordinate.longitude } : undefined}
+          showsUserLocation={!this.isAnonymous ? true : false}
+          followsUserLocation={!this.isAnonymous ? true : false}
+          onUserLocationChange={!this.isAnonymous ? 
+            (params) => {
+              this.origin = { 
+                latitude: params.nativeEvent.coordinate.latitude,
+                longitude: params.nativeEvent.coordinate.longitude 
+              };
+            } : undefined}
         >
           {this.renderSites()}
-          {/* {this.renderCurrentLocation()} */}
           {this.destination !== undefined && this.origin !== undefined ? 
           <MapViewDirections 
             origin={this.origin} 
-            destination={this.destination} 
+            d destination = {{ latitude: parseFloat(this.destination.lat), longitude: parseFloat(this.destination.lon) }}
             strokeWidth={3}
             strokeColor="#4289DD"
             apikey={GOOGLE_MAPS_APIKEY} 
@@ -198,12 +167,44 @@ export default class MapScreen extends React.Component {
             }} />
           : undefined}
         </MapView>
-        
-        
 
         <SwipeUpSearch
           onServicePress={this.filterSites}
         />
+        
+        {modalVisible ?
+          <TouchableWithoutFeedback onPress={() => this.setState({modalVisible: false})}>
+            <View style={styles.modalContainer}>
+              <TouchableWithoutFeedback>
+                <View style={styles.modal}>
+                  <View>
+                    <Text>{this.destination.name}</Text>
+                    <Text>{`\n`}Address</Text>
+                    <Text>{this.destination.street}</Text>
+                    <Text>{this.destination.province}, {this.destination.country}</Text>
+                    <Text>{this.destination.postal_code}</Text>
+                    <Text>{`\n`}Phone Number</Text>
+                    <Text>{this.destination.phone_number}</Text>
+                    <Text>{`\n`}Hours</Text>
+                    <Text>{this.destination.hours}</Text>
+                  </View>
+                  <View>
+                    <ResponsiveButton
+                      key='directions'
+                      label='Directions'
+                      labelStyle={{fontWeight: '600'}}
+                      style={styles.directionsButton}
+                      gradientColors={['#F3CB14', '#E58B37']}
+                      horizontalGradient={true}
+                      onPress={() => {this.setState({modalVisible: false}); this.setDestination()}}
+                    />
+                  </View>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        : undefined}
+
       </View>
     );
   }
@@ -214,6 +215,36 @@ const styles = StyleSheet.create({
     color: '#4289DD',
     textDecorationLine: 'underline',
     paddingTop: 5
+  },
+  modalContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingBottom: 60,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modal: {
+    position: 'relative',
+    flexDirection: 'column',
+    backgroundColor: '#FFF',
+    width: WINDOW_WIDTH * 0.8,
+    height: WINDOW_HEIGHT * 2/3,
+    padding: 20,
+    borderRadius: 30,
+    borderWidth: 3,
+    borderColor: '#E58B37',
+    justifyContent: 'space-between'
+  },
+  directionsButton: {
+    alignSelf: 'center',
+    marginBottom: -45,
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 100
   }
 });
 
